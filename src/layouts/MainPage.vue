e1f26db61106512c9940c834f9569fa78cf7d95a
crazyzpl <crazyzplzpl@163.com>
2021-11-24 15:42:39 +0800

    update

--- src/layouts/MainPage.vue
+++ src/layouts/MainPage.vue
@@ -17,7 +17,51 @@
       <router-view />
     </q-page-container>
 
-    <q-footer reveal class="page-footer" style="position: fixed; bottom: 0;">
+    <!-- <div class="page-footer" style="position: absolute; bottom: 0;">
+      <div class="footer-bg">
+        <div class="footer-content">
+          <div class="content-4">
+            <div class="content-items">
+              <q-img :src="url" class="content-logo"></q-img>
+              <div class="content-h1">{{ $t('Footer.First.Title') }}</div>
+              <div class="content-h4">{{ $t('Footer.First.Content') }}</div>
+            </div>
+            <div class="content-items">
+              <h4>{{ $t('Footer.Second.Title') }}</h4>
+              <div class="item-links">
+                <a class="footer-link">{{ $t('Footer.Second.Home') }}</a>
+                <a class="footer-link">{{ $t('Footer.Second.Products') }}</a>
+                <a class="footer-link">{{ $t('Footer.Second.WhySpacemesh') }}</a>
+                <a class="footer-link">{{ $t('Footer.Second.WhyProcyon') }}</a>
+              </div>
+            </div>
+            <div class="content-items">
+              <h4>{{ $t('Footer.Third.Title') }}</h4>
+              <div class="item-links">
+                <a class="footer-link">{{ $t('Footer.Third.Faqs') }}</a>
+                <a class="footer-link">{{ $t('Footer.Third.Support') }}</a>
+              </div>
+            </div>
+            <div class="content-items">
+              <h4>{{ $t('Footer.Forth.Title') }}</h4>
+              <div class="item-links">
+                <div class="footer-div">
+                  {{ $t('Footer.Forth.Address') }}:
+                  <span style="font-weight: bolder;">#####</span>
+                </div>
+                <div class="footer-div">
+                  {{ $t('Footer.Forth.Email') }}:
+                  <span style="font-weight: bolder;">############</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p class="copyright">© 2021 PRCN Technology Ltd.</p>
+        </div>
+      </div>
+    </div>-->
+
+    <q-footer class="page-footer" style="position: absolute; bottom: 0;">
       <div class="footer-bg">
         <div class="footer-content">
           <div class="content-4">
